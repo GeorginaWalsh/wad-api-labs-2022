7c6e73880d914b38e68ff323cb50100839d3519c
georginawalsh <20093147>
2022-11-18 15:45:19 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,54 +1,45 @@
 import express from 'express';
-import { genres } from './genresData';
-//import uniqid from 'uniqid'
+import Genre from './genreModel'
+
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
-// router.get('/:id', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     if (movieDetails.id == id) {
-//         res.status(200).json(movieDetails);
-//     } else {
-//         res.status(404).json({
-//             message: 'The resource you requested could not be found.',
-//             status_code: 404
-//         });
-//     }
-// });
+// register
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await Genre(req.body).save()
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new genre.',
+        });
+    }
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
+        const genre = await Genre.findOne(req.body);
+        if (!genre) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+}));
 
-// // Get movie reviews
-// router.get('/:id/reviews', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     // find reviews in list
-//     if (movieReviews.id == id) {
-//         res.status(200).json(movieReviews);
-//     } else {
-//         res.status(404).json({
-//             message: 'The resource you requested could not be found.',
-//             status_code: 404
-//         });
-//     }
-// });
-
-// //Post a movie review
-// router.post('/:id/reviews', (req, res) => {
-//     const id = parseInt(req.params.id);
-
-//     if (movieReviews.id == id) {
-//         req.body.created_at = new Date();
-//         req.body.updated_at = new Date();
-//         req.body.id = uniqid();
-//         movieReviews.results.push(req.body); //push the new review onto the list
-//         res.status(201).json(req.body);
-//     } else {
-//         res.status(404).json({
-//             message: 'The resource you requested could not be found.',
-//             status_code: 404
-//         });
-//     }
-// });
+ // Update
+ router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Genre.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Genre Updated Sucessfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to Update Genre' });
+    }
+});
 
 export default router;
