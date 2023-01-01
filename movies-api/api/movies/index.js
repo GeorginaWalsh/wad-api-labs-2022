770abc5708079bc16d687e7695352f633bd64c07
georginawalsh <20093147>
2023-01-01 18:47:52 +0000

    updated Movies to read from tmdb

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
+
+import { getMovies } from '../tmdb-api';
 import { getUpcomingMovies } from '../tmdb-api';
 
 import movieModel from './movieModel';
@@ -8,20 +10,25 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); 
 
-router.get('/', asyncHandler(async (req, res) => {
-    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
-    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+router.get('/', asyncHandler( async(req, res) => {
+    const movies = await getMovies();
+    res.status(200).json(movies);
+  }));
 
-    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
-    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+// router.get('/', asyncHandler(async (req, res) => {
+//     let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+//     [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
 
-    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
-    const movies = await moviesPromise;
+//     const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+//     const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
 
-    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+//     const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+//     const movies = await getMovies();
 
-    res.status(200).json(returnObject);
-}));
+//     const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+//     res.status(200).json(returnObject);
+// }));
 
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
--- reactApp/src/pages.js
+++ reactApp/src/pages.js
@@ -11,6 +11,7 @@ export const PublicPage = () => {
         <h2>Movies Data </h2>
         <div>
             {context.movies.results.map(movie => { return <>{movie.id},{movie.title}<br /></> })}
+            {/* {context.movies.results.map(movie => { return <>{movie.id},{movie.title}<br /></> })} */}
         </div>
     </>
 }
